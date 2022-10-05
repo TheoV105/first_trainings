2565528ba9a31d8b97f07511a50b4f743b1ec623
Theo Vuillard <vuillard2u@etu.univ-lorraine.fr>
2022-10-05 09:11:51 +0200

    last exercise

--- Vehicules/Vehicules.cpp
+++ Vehicules/Vehicules.cpp
@@ -16,12 +16,44 @@ void Voiture::affiche() const {
     cout << "Ceci est une voiture avec " << m_portes << " portes et coutant " << m_prix << " euros." << endl;
 }
 
+int Voiture::nbRoues() const {
+    return 4;
+}
+
 Voiture::~Voiture() {}
 
 Moto::Moto(int prix, float vitesseMax) : Vehicule(prix), m_vitesse(vitesseMax) {}
+
+int Moto::nbRoues() const {
+    return 2;
+}
 
 void Moto::affiche() const {
     cout << "Ceci est une moto allant a " << m_vitesse << " km/h et coutant " << m_prix << " euros." << endl;
 }
 
 Moto::~Moto() {}
+
+Camion::Camion(int prix, int transport) : Vehicule(prix), m_transport(transport) {}
+
+int Camion::nbRoues() const {
+    return 4;
+}
+
+void Camion::affiche() const {
+    cout << "Ceci est un camion pouvant transporter " << m_transport << " kg et coutant " << m_prix << " euros." << endl;
+}
+
+Camion::~Camion() {}
+
+TukTuk::TukTuk(int prix, int transport) : Vehicule(prix), m_transport(transport) {}
+
+int TukTuk::nbRoues() const {
+    return 3;
+}
+
+void TukTuk::affiche() const {
+    cout << "Ceci est un tuk-tuk pouvant transporter " << m_transport << " kg et coutant " << m_prix << " euros." << endl;
+}
+
+TukTuk::~TukTuk() {}
--- Vehicules/Vehicules.hpp
+++ Vehicules/Vehicules.hpp
@@ -1,34 +1,62 @@
 #ifndef VEHICULES_HPP
 #define VEHICULES_HPP
 
+class Garage {
+
+};
+
 class Vehicule {
     public:
     Vehicule(int prix);
     virtual void affiche() const;  //Affiche une description du Vehicule
-    ~Vehicule();
+    virtual int nbRoues() const = 0;
+    void affichePrix() const;
+    virtual ~Vehicule();
 
     protected:
     int m_prix;  //Chaque véhicule a un prix
+    int m_annee;
 };
+        class Voiture : public Vehicule {
+            public:
+            Voiture(int prix, int portes);
+            virtual void affiche() const;
+            virtual int nbRoues() const;
+            virtual ~Voiture();
 
-class Voiture : public Vehicule {
-    public:
-    Voiture(int prix, int portes);
-    virtual void affiche() const;
-    ~Voiture();
+            private:
+            int m_portes;  //Le nombre de portes de la voiture
+        };
 
-    private:
-    int m_portes;  //Le nombre de portes de la voiture
-};
+        class Moto : public Vehicule {
+            public:
+            Moto(int prix, float vitesse);
+            virtual void affiche() const;
+            virtual int nbRoues() const;
+            virtual ~Moto();
+        
+            private:
+            float m_vitesse;  //La vitesse maximale de la moto
+        };
 
-class Moto : public Vehicule {
-    public:
-    Moto(int prix, float vitesse);
-    virtual void affiche() const;
-    ~Moto();
- 
-    private:
-    float m_vitesse;  //La vitesse maximale de la moto
-};
+        class Camion : public Vehicule {
+            public:
+            Camion(int prix, int transport);
+            virtual void affiche() const; ///////////////////
+            virtual int nbRoues() const;
+            virtual ~Camion();
+            private:
+            int m_transport;
+        };
+
+        class TukTuk : public Vehicule {
+            public:
+            TukTuk(int prix, int transport);
+            virtual void affiche() const; ///////////////////
+            virtual int nbRoues() const;
+            virtual ~TukTuk();
+            private:
+            int m_transport;
+        };
 
 #endif
